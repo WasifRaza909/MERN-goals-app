4b7b5e60f1db884a2ffc38cea059a6bd82114cc1
wasifraza909 <wasifraza909@gmail.com>
2023-05-07 00:54:24 +0500

    Rest API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log('Hello World !')
+const express = require("express");
+const colors = require('colors')
+const connectDB = require('./config/db')
+const {errorHandler} = require("./middlewares/errorMiddleware");
+const dotenv = require("dotenv").config()
+
+const port  = process.env.PORT ||5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use("/api/goals", require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+
+app.listen(port, () => console.log(`Server is started on port ${port}`))
