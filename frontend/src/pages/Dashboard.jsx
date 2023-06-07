c4b0463ccdaad46305cb2d810cb196c4c9cdae0e
wasifraza909 <wasifraza909@gmail.com>
2023-06-07 09:46:47 +0500

    Working

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,49 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import GoalForm from "../components/GoalForm";
+import { getGoals, reset } from "../features/goals/goalSlice";
+import Spinner from "../components/Spinner";
+
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.goals
+  );
+
+  useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
+
+    if (!user) {
+      navigate("/login");
+    }
+    
+    dispatch(getGoals());
+
+    // It is used when you want to do something when component is dismount
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+
+      <GoalForm />
+    </>
+  );
 }
 
 export default Dashboard;
